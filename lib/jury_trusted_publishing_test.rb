ee58a935147dd438e0073a5438ab91aca0bf6a72
Jury Razumau <jury.razumau@zendesk.com>
2024-06-27 13:06:18 +0200

    generate gem
    `bundle gem --ci=github --coc --no-mit --test=minitest --linter=standard jury_trusted_publishing_test`

---
+++ lib/jury_trusted_publishing_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jury_trusted_publishing_test/version"
+
+module JuryTrustedPublishingTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
